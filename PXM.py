f98459636d831d6f078d19c53d74de4a744ce662
radekrudak <40768938+radekrudak@users.noreply.github.com>
2021-11-25 20:04:03 +0100

    BUG FIXES

--- PXM.py
+++ PXM.py
@@ -1,3 +1,4 @@
+import numpy
 class value_too_big_error(Exception):
     pass
 
@@ -8,6 +9,9 @@ class pxm_file():
        "P1":"P1",
        "P2":"P2",
        "P3":"P3",
+       "P4":"P4",
+       "P5":"P5",
+       "P6":"P6",
         "pbm":"P1",
         ".pbm":"P1",
         "pgm":"P2",
@@ -20,40 +24,67 @@ class pxm_file():
     height:int
     max_value:float
     matrix= [[]]
-    def __init__(self,width:int,height:int,default_value,max_value,what_type:str):
+    comment:str
+    def __init__(self,width:int, height:int, default_value, max_value, what_type:str):
         self.width = width
         self.height =height
         self.max_value = max_value
-        self.matrix = [[default_value]*self.width]*self.height
+        self.matrix = numpy.ones((self.height,self.width),numpy.int8)
+        self.matrix *= default_value
         self.what_type = self.type_dyctionary[what_type]
         if self.type_dyctionary[what_type] == 'P1':
             self.max_value = 1
-    def SetPixel(self,x,y,val):
+    def set_pixel(self,x,y,val:int):
         if val<= self.max_value:
-            matrix[y][x] = val 
+            self.matrix[y][x] = val 
         else:
             raise value_too_big_error ("Given value is bgger than max value you chosen")       
 
-    def GetPixel(self,x,y):
+    def get_pxiel(self,x,y):
         return self.matrix[y][x]
-    def SaveToFile(self,path:str):
-        file = open(path, 'w')
-        file.write('')
-        file.close()
+    
 
-        file = open(path,'a')
+            
+    def save_to_file(self,path:str, comment=''):
+        self.comment = comment
 
-        file.write(self.what_type+'\n')
-        file.write(str(self.width)+" "+str(self.height)+'\n')
-        file.write(str(self.max_value)+'\n')
+        if int(self.what_type[1]) >=3:
+            file = open(path, 'w')
+            file.write('')
+            file.close()
 
-        for y in range(self.height):
-            for x in range(self.width):
-                file.write(str(self.GetPixel(x,y)))
-                file.write(' ')
-            file.write('\n')
+            file = open(path,'ba')
+            
+            file.write(bytes(self.what_type+'\n', 'utf-8'))
+            file.write(bytes('#'+self.comment+"\n", 'utf-8'))
+            file.write(bytes(str(self.width)+" "+str(self.height)+'\n', 'utf-8'))
+            file.write(bytes(str(self.max_value)+'\n', 'utf-8'))
+
+                    
+            for y in range(self.height):
+                    for x in range(self.width):
+                        file.write(bytes(self.get_pxiel(x,y)))
+
+        else:
+            file = open(path, 'w')
+            file.write('')
+            file.close()
+
+            file = open(path,'a')
+            
+            file.write(self.what_type+'\n')
+            file.write('#'+self.comment+"\n")
+            file.write(str(self.width)+" "+str(self.height)+'\n')
+            file.write(str(self.max_value)+'\n')
+
+                        
+                
+        
+            for y in range(self.height):
+                for x in range(self.width):
+                    file.write(str(self.get_pxiel(x,y)))
+                    file.write(' ')
+                file.write('\n')
 
 
-        file.close()
-
-
+            file.close()
